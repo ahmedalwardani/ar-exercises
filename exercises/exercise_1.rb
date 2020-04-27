22efe400035e12809bd641ab831894989f87e599
ahmedalwardani <aalwa049@uottawa.ca>
2020-04-27 21:51:03 +0000

    create stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,30 @@
 require_relative '../setup'
 
-puts "Exercise 1"
-puts "----------"
+puts 'Exercise 1'
+puts '----------'
 
-# Your code goes below here ...
+store1 =
+  Store.create(
+    name: 'Burnaby',
+    annual_revenue: 300_000,
+    mens_apparel: true,
+    womens_apparel: true
+  )
+
+store2 =
+  Store.create(
+    name: 'Richmond',
+    annual_revenue: 1_260_000,
+    womens_apparel: true,
+    mens_apparel: false
+  )
+
+store3 =
+  Store.create(
+    name: 'Gastown',
+    annual_revenue: 190_000,
+    mens_apparel: true,
+    womens_apparel: false
+  )
+
+puts Store.count
